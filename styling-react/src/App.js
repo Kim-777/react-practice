2af9ae5fe025c723466650a89222dd8407c9f278
Kim-777 <iloo3012@naver.com>
2021-04-07 17:48:44 +0900

    practice how to style component in react

---
+++ styling-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import SassComponent from './components/SassComponent';
+import CSSModule from './components/CSSModule';
+import StyledComponent from './components/StyledComponent';
+
+
+function App() {
+  return (
+    <>
+      <SassComponent />
+      <hr />
+      <CSSModule />
+      <hr />
+      <StyledComponent />
+    </>
+  );
+}
+
+export default App;
