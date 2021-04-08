0042b8097b837915582cb02fbd70d9aede1480e2
Kim-777 <iloo3012@naver.com>
2021-04-08 14:42:03 +0900

    context-practice

---
+++ context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ColorContext from '../contexts/color';
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+class SelectColors extends Component {
+
+    static contextType = ColorContext;
+
+    render() {
+        return (
+            <div>
+                <h2>색상을 선택하세요.</h2>
+                    <div style={{display: 'flex'}}>
+                        {
+                            colors.map(color => (
+                                <div
+                                    key={color}
+                                    style={{background: color, width: '24px', height: '24px', cursor: 'pointer'}}
+                                    onClick={() => this.context.actions.setColor(color)}
+                                    onContextMenu={e => {
+                                        e.preventDefault();
+                                        this.context.actions.setSubcolor(color);
+                                    }}
+                                />
+                            ))
+                        }
+                    </div>
+                <hr />
+            </div>
+        )
+    }
+} 
+
+export default SelectColors;
