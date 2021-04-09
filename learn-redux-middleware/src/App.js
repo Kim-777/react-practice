1253ce3e7e516729d55a9437c63a438eddcb6c67
Kim-777 <iloo3012@naver.com>
2021-04-09 17:02:55 +0900

    practice redux-middleware

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import CounterContainer from './containers/CounterContainer';
+import './App.css';
+import SampleContainer from './containers/SampleContainer';
+
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;
