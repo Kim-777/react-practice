3c0d36d014f8c3cd680e6813b87d2aef80c7b0fa
Kim-777 <iloo3012@naver.com>
2021-05-05 22:41:35 +0900

    todo-app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,60 +4,14 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function createBulkTodos() {
-  const array = [];
-  for(let i =1; i<= 500; i++) {
-    array.push({
-      id: i,
-      text: `할 일 ${i}`,
-      checked: false,
-    })
-  }
-
-  return array;
-}
 
 function App() {
-
-  const [todos, setTodos] = useState(createBulkTodos);
-
-  const nextId = useRef(501);
-
-  const onInsert = useCallback(
-  text => {
-    const todo = {
-      id: nextId.current,
-      text,
-      checked: false,
-    }
-    setTodos(todos => todos.concat(todo));
-  },
-  [],
-  )
-
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => 
-        todos.filter(todo => todo.id !== id)
-      )
-    },
-    [],
-  )
-
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos => 
-        todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo)
-      )
-    },
-    [],
-  )
 
   return (
     <div>
       <TodoTemplate>
-          <TodoInsert onInsert={onInsert}/>
-          <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+        <TodoInsert />
+        <TodoList />
       </TodoTemplate>
     </div>
   );
